5982f79eb107d48295e24a150d58990b2422ac50
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-21 02:47:44 +0530

    Added Generator Loss

--- src/loss.py
+++ src/loss.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 
 
 class TotalVarianceLoss(torch.nn.Module):
@@ -20,3 +21,27 @@ class TotalVarianceLoss(torch.nn.Module):
         h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
         w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
         return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+
+class GeneratorLoss(torch.nn.Module):
+
+    def __init__(self):
+        super(GeneratorLoss, self).__init__()
+        vgg16 = torchvision.models.vgg.vgg16(pretrained=True)
+        network = torch.nn.Sequential(*list(vgg16.features)[:31]).eval()
+        for param in network.parameters():
+            param.requires_grad = False
+        self.network = network
+        self.mse = torch.nn.MSELoss()
+        self.tv = TotalVarianceLoss()
+
+    def forward(self, labels, images, targets):
+        adversarial_loss = torch.mean(1 - labels)
+        perception_loss = self.mse(
+            self.network(images),
+            self.network(targets)
+        )
+        image_loss = self.mse(images, targets)
+        tv_loss = self.tv(images)
+        loss = image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+        return loss

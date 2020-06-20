986d671bab4e1fd459329424969457687f346b3c
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-21 01:55:10 +0530

    Added Upsample Block

--- src/models/blocks.py
+++ src/models/blocks.py
@@ -24,3 +24,22 @@ class ResidualBlock(torch.nn.Module):
         y = self.conv_2(y)
         y = self.batch_norm_2(y)
         return x + y
+
+
+class UpSampleBlock(torch.nn.Module):
+
+    def __init__(self, channels, scale):
+        super(UpSampleBlock, self).__init__()
+        self.conv = torch.nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels * (scale ** 2),
+            kernel_size=3, padding=1
+        )
+        self.pixel_shuffle = torch.nn.PixelShuffle(scale)
+        self.activation = torch.nn.PReLU()
+
+    def forward(self, x):
+        y = self.conv(x)
+        y = self.pixel_shuffle(y)
+        y = self.activation(y)
+        return y

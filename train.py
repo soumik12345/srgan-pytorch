6625a0d131745ba3f586ea4a6b4b10623d8ab20f
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-21 13:18:02 +0530

    Added Discriminator training

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm import tqdm
 from src.loss import GeneratorLoss
 from src.models import Generator, Discriminator
 from src.dataset import TrainDataset, ValidationDataset
@@ -12,6 +13,13 @@ class Trainer:
         self.generator, self.discriminator = self.get_models()
         self.generator_criterion = GeneratorLoss().cuda()
         self.generator_optimizer, self.discriminator_optimizer = self.get_optimizers()
+        self.results = {
+            'discriminator_loss': [],
+            'generator_loss': [],
+            'discriminator_score': [],
+            'generator_score': [],
+            'psnr': [], 'ssim': []
+        }
 
     def get_dataloaders(self):
         train_dataset = TrainDataset(
@@ -39,3 +47,23 @@ class Trainer:
         generator_optimizer = torch.optim.Adam(self.generator.parameters())
         discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters())
         return generator_optimizer, discriminator_optimizer
+
+    def train_discriminator(self, data, target, batch_size):
+        """maximize D(x)-1-D(G(z))"""
+
+        real_image = torch.autograd.Variable(target).cuda()
+        z = torch.autograd.Variable(data).cuda()
+        fake_image = self.generator(z)
+
+        self.discriminator.zero_grad()
+
+        real_output = self.discriminator(real_image).mean()
+        fake_output = self.discriminator(fake_image).mean()
+
+        discriminator_loss = 1 - real_output + fake_output
+        discriminator_loss.backward(retain_graph=True)
+
+    def train_step(self):
+        for data, target in tqdm(self.train_dataset):
+            batch_size = data.size(0)
+            self.train_discriminator(data, target, batch_size)

078fd20be5a9644d0be025bef9b501988391e654
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-26 01:14:35 +0530

    Removed broken script

--- data/voc_2012.sh
+++
@@ -1,7 +0,0 @@
-mkdir /root/.kaggle
-touch /root/.kaggle/kaggle.json
-chmod 600 /root/.kaggle/kaggle.json
-echo "{\"username\":\"<YOUR-USERNAME>\",\"key\":\"<YOUR-KAGGLE-KEY>\"}" > /root/.kaggle/kaggle.json
-kaggle datasets download -d huanghanchina/pascal-voc-2012
-unzip -q ./pascal-voc-2012.zip
-rm ./pascal-voc-2012.zip
